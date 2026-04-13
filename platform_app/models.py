91f8df167fa14bdf9236e3908b73784842eca81f
Ibeawuchi chukwugozirim <ibeawuchichukwugozirim@gmail.com>
2026-04-13 18:36:43 +0100

    changes

--- platform_app/models.py
+++ platform_app/models.py
@@ -50,6 +50,8 @@ class WalletAddress(models.Model):
         ('XRP', 'Ripple'),
         ('ADA', 'Cardano'),
         ('DOGE', 'Dogecoin'),
+        ('USDC (Sol)', 'Dogecoin'),
+        ('USDC (Eth)', 'Dogecoin'),
     ]
     
     cryptocurrency = models.CharField(max_length=10, choices=CRYPTO_CHOICES, unique=True)
