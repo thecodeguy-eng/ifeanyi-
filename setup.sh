982db3266e9d8ced8db65e586597e2262dab9450
Ibeawuchi chukwugozirim <ibeawuchichukwugozirim@gmail.com>
2026-09-10 00:44:07 +0100

    update

--- setup.sh
+++ setup.sh
@@ -7,4 +7,4 @@ pip install -r requirements.txt
 # Run django commands
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic
+python manage.py collectstatic --noinput
