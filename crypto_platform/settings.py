fb26c91139552fa4d2a983c9e93fd3189c52cab0
Ibeawuchi chukwugozirim <ibeawuchichukwugozirim@gmail.com>
2026-09-10 00:47:37 +0100

    update

--- crypto_platform/settings.py
+++ crypto_platform/settings.py
@@ -11,8 +11,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-your-secret-key-here')
  
-# DEBUG = True
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
+# DEBUG = config('DEBUG', default=False, cast=bool)
 
 # ALLOWED_HOSTS
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', 'localhost', '127.0.0.1', '.com']
